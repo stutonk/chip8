9dfe22d10a6091a3aeac6f498cd184a4c1edf207
stutonk <xealblade@gmail.com>
2019-01-21 16:54:38 -0500

    Implement input handling

--- chip8.c
+++ chip8.c
@@ -8,14 +8,19 @@
 #define INSTR_SZ 2
 #define NUMREGS 16
 
+#define CASE_CHIP8_KEY_RETURN(N) \
+    case CHIP8_KEY_ ## N:        \
+        return 0x ## N;
+
 static uint8_t mem[CHIP8_MEM_SZ] = {0};
 static uint16_t stack[CHIP8_STACK_SZ] = {0};
 static uint8_t reg_v[NUMREGS] = {0};
 static uint16_t reg_i = 0;
 static uint16_t pc = 0;
 static size_t sp = 0;
+static uint16_t key_state = 0;
 
-static void key(uint8_t, uint8_t);
+static uint8_t key_to_num(SDL_Keycode);
 static void op_0(uint8_t);
 static void op_8(uint8_t, uint8_t, uint8_t);
 static void op_f(uint8_t, uint8_t);
@@ -38,6 +43,7 @@ void chip8_reset(void)
     reg_i = 0;
     pc = 0;
     sp = 0;
+    key_state = 0;
     screen_cls();
 }
 
@@ -58,12 +64,20 @@ bool chip8_load(uint16_t offset, uint8_t bytes[], size_t num)
 void chip8_execute(uint16_t entry)
 {
     SDL_Event e = {0};
-    for (pc = entry; pc < CHIP8_MEM_SZ; pc += INSTR_SZ) {
-        if (SDL_PollEvent(&e) 
-            && e.type == SDL_KEYUP 
-            && e.key.keysym.sym == CHIP8_QUIT_KEY
-        ) {
-            break;
+    for (pc = entry; pc < CHIP8_MEM_SZ - INSTR_SZ; pc += INSTR_SZ) {
+        if (SDL_PollEvent(&e)) {
+            if (e.type == SDL_KEYUP) {
+                if (e.key.keysym.sym == CHIP8_KEY_QUIT) {
+                    return;
+                }
+                uint8_t keynum = key_to_num(e.key.keysym.sym);
+                if (keynum < CHIP8_NUMKEYS) {
+                    key_state |= (0x1 << keynum);
+                }
+            }
+            else if (e.type == SDL_QUIT) {
+                return;
+            }
         }
         
         uint8_t op_hi = mem[pc];
@@ -133,7 +147,26 @@ void chip8_execute(uint16_t entry)
                 );
                 break;
             case 0xe: //Skip if [not] key
-                key(op_lo, reg_operand_x);
+                {
+                    uint16_t xpressed = key_state & (0x1 << reg_operand_x);
+                    switch (op_lo) {
+                        case 0x9e:
+                            if (xpressed) {
+                                pc += INSTR_SZ;
+                                key_state = 0;
+                            }
+                            break;
+                        case 0xa1:
+                            if (!xpressed) {
+                                pc += INSTR_SZ;
+                                key_state = 0;
+                            }
+                            break;
+                        default:
+                            FAIL("illegal instruction");
+                            break;
+                    }
+                }
                 break;
             case 0xf: //Misc ops
                 op_f(op_lo, reg_operand_x);
@@ -145,7 +178,27 @@ void chip8_execute(uint16_t entry)
     }
 }
 
-static inline void key(uint8_t op, uint8_t reg) {}
+static uint8_t key_to_num(SDL_Keycode key) {
+    switch (key) {
+        CASE_CHIP8_KEY_RETURN(0)
+        CASE_CHIP8_KEY_RETURN(1)
+        CASE_CHIP8_KEY_RETURN(2)
+        CASE_CHIP8_KEY_RETURN(3)
+        CASE_CHIP8_KEY_RETURN(4)
+        CASE_CHIP8_KEY_RETURN(5)
+        CASE_CHIP8_KEY_RETURN(6)
+        CASE_CHIP8_KEY_RETURN(7)
+        CASE_CHIP8_KEY_RETURN(8)
+        CASE_CHIP8_KEY_RETURN(9)
+        CASE_CHIP8_KEY_RETURN(A)
+        CASE_CHIP8_KEY_RETURN(B)
+        CASE_CHIP8_KEY_RETURN(C)
+        CASE_CHIP8_KEY_RETURN(D)
+        CASE_CHIP8_KEY_RETURN(E)
+        CASE_CHIP8_KEY_RETURN(F)
+    }
+    return CHIP8_NUMKEYS;
+}
 
 static void op_0(uint8_t op) 
 {
@@ -154,14 +207,15 @@ static void op_0(uint8_t op)
             screen_cls();
             break;
         case 0xee: //RET
-            pc = stack[sp];
-            --sp;
-            if (sp > CHIP8_STACK_SZ) {
+            if (sp >= 0) {
                 FAIL("stack underflow");
             }
+            pc = stack[sp];
+            --sp;
             break;
         default:
             FAIL("illegal instruction");
+            break;
     }
 }
 
@@ -183,18 +237,14 @@ static void op_8(uint8_t op, uint8_t x, uint8_t y)
         case 0x4: //ADD
             {
                 uint8_t res = reg_v[x] + reg_v[y];
-                if (res < reg_v[x] || res < reg_v[y]) {
-                    reg_v[0xf] = 0x1;
-                }
+                reg_v[0xf] = (res < reg_v[x] || res < reg_v[y]) ? 0x1 : 0x0;
                 reg_v[x] = res;
             }
             break;
         case 0x5: //SUBY
             {
                 uint8_t res = reg_v[x] - reg_v[y];
-                if (res > reg_v[x]) {
-                    reg_v[0xf] = 0x1;
-                }
+                reg_v[0xf] = (res > reg_v[x]) ? 0x1 : 0x0;
                 reg_v[x] = res;
             }
             break;
@@ -205,18 +255,63 @@ static void op_8(uint8_t op, uint8_t x, uint8_t y)
         case 0x7: //SUBX
             {
                 uint8_t res = reg_v[y] - reg_v[x];
-                if (res > reg_v[y]) {
-                    reg_v[0xf] = 0x1;
-                }
+                reg_v[0xf] = (res > reg_v[y]) ? 0x1 : 0x0;
                 reg_v[x] = res;
             }
             break;
         default:
             FAIL("illegal instruction");
+            break;
     }
 }
 
 static void op_f(uint8_t op, uint8_t x) 
 {
-    FAIL("unimplemented");
+    switch (op) {
+        case 0x07: //GDEL
+            FAIL("unimplemented");
+            break;
+        case 0x0a: //GKEY
+            {
+                SDL_Event e = {0};
+                while (SDL_WaitEvent(&e)) {
+                    if (e.type == SDL_KEYUP) {
+                        uint8_t keynum = key_to_num(e.key.keysym.sym);
+                        if (keynum < CHIP8_NUMKEYS) {
+                            reg_v[x] = keynum;
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+        case 0x15: //SDEL
+            FAIL("unimplemented");
+            break;
+        case 0x18: //SSND
+            FAIL("unimplemented");
+            break;
+        case 0x1e:
+            {
+                uint16_t res = reg_i + reg_v[x];
+                reg_v[0xf] = (res < reg_i) ? 0x1 : 0x0;
+                reg_i = res;
+            }
+            break;
+        case 0x29: //CHRX
+            FAIL("unimplemented");
+            break;
+        case 0x33: //BCD
+            FAIL("unimplemented");
+            break;
+        case 0x55: //REGD
+            FAIL("unimplemented");
+            break;
+        case 0x65: //REGL
+            FAIL("unimplemented");
+            break;
+        default:
+            FAIL("illegal instruction");
+            break;
+    }
 }
--- chip8.h
+++ chip8.h
@@ -5,7 +5,27 @@
 #include <stdint.h>
 #include <SDL.h>
 
-#define CHIP8_QUIT_KEY SDLK_ESCAPE
+
+#define CHIP8_KEY_QUIT SDLK_ESCAPE
+/* Assuming QWERTY layout */
+#define CHIP8_NUMKEYS 16
+#define CHIP8_KEY_1 SDLK_1
+#define CHIP8_KEY_2 SDLK_2
+#define CHIP8_KEY_3 SDLK_3
+#define CHIP8_KEY_C SDLK_4
+#define CHIP8_KEY_4 SDLK_q
+#define CHIP8_KEY_5 SDLK_w
+#define CHIP8_KEY_6 SDLK_e
+#define CHIP8_KEY_D SDLK_r
+#define CHIP8_KEY_7 SDLK_a
+#define CHIP8_KEY_8 SDLK_s
+#define CHIP8_KEY_9 SDLK_d
+#define CHIP8_KEY_E SDLK_f
+#define CHIP8_KEY_A SDLK_z
+#define CHIP8_KEY_0 SDLK_x
+#define CHIP8_KEY_B SDLK_c
+#define CHIP8_KEY_F SDLK_v
+
 #define CHIP8_MEM_SZ 4096
 #define CHIP8_STACK_SZ 50
 
